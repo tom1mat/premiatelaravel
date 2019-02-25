1f6e53a05c23354ad415b8eed88318ff6cee145f
Tomi Mateo <tomasmateo10@gmail.com>
2019-02-25 00:28:14 -0300

    Se agrego panel de LOG IN, middleware al panel y cambio de variables de AUTH para adaptandolas al template

---
+++ resources/views/panel/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.panel')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="block block-rounded">
+                <div class="block-content">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            Bienvenido al panel de administración
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,32 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');;
 
 Route::prefix('panel')->group(function () {
-    Route::prefix('usuarios')->group(function () {
-        Route::get('/', 'UsersController@index')->name("usuarios.index");;
-        Route::get('create', 'UsersController@create')->name("usuarios.create");;
-        Route::get('edit/{id}', 'UsersController@edit')->name("usuarios.edit");
-    });
-    Route::prefix('premios')->group(function () {
-        Route::get('/', 'PremiosController@index')->name("premios.index");
-        Route::get('create', 'PremiosController@create')->name("premios.create");
-        Route::get('edit/{id}', 'PremiosController@edit')->name("premios.edit");
-        Route::post('store', 'PremiosController@store')->name("premios.store");
-        Route::put('update/{id}', 'PremiosController@update')->name("premios.update");
-        Route::get('destroy/{id}', 'PremiosController@destroy')->name("premios.destroy");
-    });
-    Route::prefix('sorteos')->group(function () {
-        Route::get('/', 'SorteosController@index')->name("sorteos.index");
-        Route::get('create', 'SorteosController@create')->name("sorteos.create");
-        Route::get('edit/{id}', 'SorteosController@edit')->name("sorteos.edit");
-        Route::post('store', 'SorteosController@store')->name("sorteos.store");
-        Route::put('update/{id}', 'SorteosController@update')->name("sorteos.update");
-        Route::get('destroy/{id}', 'SorteosController@destroy')->name("sorteos.destroy");
+    Route::group(['middleware' => ['auth']], function () {
+        Route::prefix('usuarios')->group(function () {
+            Route::get('/', 'UsersController@index')->name("usuarios.index");
+            Route::get('create', 'UsersController@create')->name("usuarios.create");
+            Route::get('edit/{id}', 'UsersController@edit')->name("usuarios.edit");
+        });
+        Route::prefix('premios')->group(function () {
+            Route::get('/', 'PremiosController@index')->name("premios.index");
+            Route::get('create', 'PremiosController@create')->name("premios.create");
+            Route::get('edit/{id}', 'PremiosController@edit')->name("premios.edit");
+            Route::post('store', 'PremiosController@store')->name("premios.store");
+            Route::put('update/{id}', 'PremiosController@update')->name("premios.update");
+            Route::get('destroy/{id}', 'PremiosController@destroy')->name("premios.destroy");
+        });
+        Route::prefix('sorteos')->group(function () {
+            Route::get('/', 'SorteosController@index')->name("sorteos.index");
+            Route::get('create', 'SorteosController@create')->name("sorteos.create");
+            Route::get('edit/{id}', 'SorteosController@edit')->name("sorteos.edit");
+            Route::post('store', 'SorteosController@store')->name("sorteos.store");
+            Route::put('update/{id}', 'SorteosController@update')->name("sorteos.update");
+            Route::get('destroy/{id}', 'SorteosController@destroy')->name("sorteos.destroy");
+        });
     });
 });
 
-
-
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
